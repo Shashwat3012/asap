4e67213f7a33d57e6d6797ca1b033e1de82e3875
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2023-02-23 14:00:03 +0530

    Basic GUI

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-from tkinter import*
+from tkinter import *
 import tkinter as tk
+
+import customtkinter
 import mysql.connector as mysql
 from tkinter import ttk, messagebox
+
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("800x500")
+
+
+def login():
+    print("Login Successful")
+
+
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = ctk.CTkLabel(master=frame, text="login System")
+label.pack(pady=12, padx=10)
+
+entry1 = ctk.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = ctk.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
