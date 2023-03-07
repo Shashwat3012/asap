d5acbf58c920b3a050a4ca1b4b1bbef15d3911d9
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-03-07 20:51:41 +0530

    Integrating main logic and voice commands

---
+++ long_responses.py
@@ -0,0 +1,12 @@
+import random
+
+R_EATING = "I like eating wires and PCBs coz I am a bot!"
+
+
+def unknown():
+    response = ['Could you please re-phrase that?',
+                "...",
+                "Sounds about right",
+                "What does that mean?"][random.randrange(4)]
+
+    return response
